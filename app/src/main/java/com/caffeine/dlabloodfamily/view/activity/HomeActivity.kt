ef4d397267f206b7ee91918d00e4587b25ab62ce
Saad Ahmed <saadahmedscse@gmail.com>
2022-04-05 20:10:24 +0600

    Initial Commit

---
+++ app/src/main/java/com/caffeine/dlabloodfamily/view/activity/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.caffeine.dlabloodfamily.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.caffeine.dlabloodfamily.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
