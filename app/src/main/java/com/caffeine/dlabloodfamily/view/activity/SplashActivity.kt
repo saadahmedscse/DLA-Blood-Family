7f635dee7a92d13dc90a71d8cbb71c99f32919c4
Saad Ahmed <saadahmedscse@gmail.com>
2022-04-06 08:14:00 +0600

    Check data before changing activity

--- app/src/main/java/com/caffeine/dlabloodfamily/view/activity/SplashActivity.kt
+++ app/src/main/java/com/caffeine/dlabloodfamily/view/activity/SplashActivity.kt
@@ -4,16 +4,52 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.activity.viewModels
 import com.caffeine.dlabloodfamily.R
 import com.caffeine.dlabloodfamily.utils.Constants
+import com.caffeine.dlabloodfamily.utils.DataState
+import com.caffeine.dlabloodfamily.viewmodel.AuthViewModel
 
 class SplashActivity : AppCompatActivity() {
+
+    private val viewModel : AuthViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            Constants.intentToActivity(this, AuthenticationActivity::class.java)
-        }, 0)
+        if (Constants.auth.currentUser != null){
+            if (Constants.internetAvailable(this)){
+                viewModel.checkData()
+            }
+            else {
+                Constants.showSnackBar(this, window.decorView, "Check your internet connection", Constants.SNACK_LONG)
+            }
+        }
+
+        else {
+            Handler(Looper.getMainLooper()).postDelayed({
+                Constants.intentToActivity(this, AuthenticationActivity::class.java)
+            }, 1500)
+        }
+
+        viewModel.authLiveData.observe(this){
+            when (it) {
+                is DataState.Loading -> {}
+
+                is DataState.Success -> {
+                    if (it.data == "noInfo") {
+                        Constants.intentToActivity(this, AuthenticationActivity::class.java)
+                    }
+                    if (it.data == "hasInfo"){
+                        Constants.intentToActivity(this, HomeActivity::class.java)
+                    }
+                }
+
+                is DataState.Failed -> {
+                    Constants.showSnackBar(this, window.decorView, it.message!!, Constants.SNACK_LONG)
+                }
+            }
+        }
     }
 }
