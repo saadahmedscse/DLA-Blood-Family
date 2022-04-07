fb8cc9aea34e86d9939ad49f583f04419f8fa773
Saad Ahmed <saadahmedscse@gmail.com>
2022-04-07 21:31:48 +0600

    Bottom navigation view with NavController and AppBar

--- app/src/main/java/com/caffeine/dlabloodfamily/view/activity/HomeActivity.kt
+++ app/src/main/java/com/caffeine/dlabloodfamily/view/activity/HomeActivity.kt
@@ -2,11 +2,28 @@ package com.caffeine.dlabloodfamily.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.caffeine.dlabloodfamily.R
+import com.caffeine.dlabloodfamily.databinding.ActivityHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavView.setupWithNavController(navController)
+    }
+
+    fun setAppBarTitle(title : String){
+        binding.appbar.text = title
     }
 }
