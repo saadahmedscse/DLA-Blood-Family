435daff99e3cfe5c9673569cf3dc23ee98918523
Saad Ahmed <saadahmedscse@gmail.com>
2022-04-07 21:30:15 +0600

    Post Interface

---
+++ app/src/main/java/com/caffeine/dlabloodfamily/services/repository/PostInterface.kt
@@ -0,0 +1,14 @@
+package com.caffeine.dlabloodfamily.services.repository
+
+import androidx.lifecycle.MutableLiveData
+import com.caffeine.dlabloodfamily.services.model.BloodModel
+import com.caffeine.dlabloodfamily.utils.DataState
+
+interface PostInterface {
+
+    fun postARequest(bloodModel: BloodModel, bloodMutableLiveData: MutableLiveData<DataState<String>>)
+
+    suspend fun getPosts(bloodMutableLiveData: MutableLiveData<DataState<ArrayList<BloodModel>>>)
+
+    fun deletePost(bloodModel: BloodModel)
+}
